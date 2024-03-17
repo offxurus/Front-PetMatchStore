d2750fc15eabbccb7a1e1f3a9ae1e95c14656011
Breno Bastos <breno17contato@hotmail.com>
2024-03-17 00:32:33 -0300

    subindo tela de cadastro de produto

--- src/app/pages/create-product/create-product.module.ts
+++ src/app/pages/create-product/create-product.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { CreateProductComponent } from './create-product.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [CreateProductComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ]
 })
 export class CreateProductModule { }
